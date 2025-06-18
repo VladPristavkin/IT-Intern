bdc90b9f87a0c6e675a29a8a16b8a71626f10690
Anastasiya Machulskaya <94453559+Lossteg@users.noreply.github.com>
2025-06-18 02:35:29 +0200

    fix rightsidebar btn

--- client/src/components/RightSideBar/RightSideBar.jsx
+++ client/src/components/RightSideBar/RightSideBar.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
 import './RightSideBar.css';
 import BRU from '../../assets/BRU.svg'
+import { useNavigate } from 'react-router-dom';
+
 
 const RightSideBar = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+      navigate(`/student`);
+  }
+
   return (
     <div className="right-side-bar">
       <div className="advertisement">
@@ -14,7 +21,7 @@ const RightSideBar = () => {
       <div className="quick-profile">
         <h3>🚀Быстрый подбор</h3>
         <p>Пройдите тестирование и мы подберём Вам вакансии индивидуально!</p>
-        <button>Перейти в профиль</button>
+        <button onClick={handleClick}>Перейти в профиль</button>
       </div>
     </div>
   );
