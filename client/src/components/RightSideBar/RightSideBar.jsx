048773961e9138fd51213dba0656eb23c531671f
Anastasiya Machulskaya <94453559+Lossteg@users.noreply.github.com>
2025-06-18 02:27:37 +0200

    fix right side bar

--- client/src/components/RightSideBar/RightSideBar.jsx
+++ client/src/components/RightSideBar/RightSideBar.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import './RightSideBar.css';
+import BRU from '../../assets/BRU.svg'
 
 const RightSideBar = () => {
   return (
     <div className="right-side-bar">
       <div className="advertisement">
-        <h3>Тут будет реклама</h3>
-        <p>Ut esse eiusmod aute. Sit enim labore dolore. Aute ea fugiat commodo ea foes.</p>
+        <img
+          src={BRU}
+          className='bru-logo'
+        />
       </div>
-
-      <div className="newsletter-subscription">
-        <h3>Подписаться на новости</h3>
-        <input type="email" placeholder="name@mail.com" />
-        <button>Подписаться</button>
-      </div>
-
       <div className="quick-profile">
-        <h3>Быстрый подбор</h3>
-        <p>Quis eiusmod deserunt cillum laboris magna cupidatat esse labore irure quis cupidatat in.</p>
+        <h3>🚀Быстрый подбор</h3>
+        <p>Пройдите тестирование и мы подберём Вам вакансии индивидуально!</p>
         <button>Перейти в профиль</button>
       </div>
     </div>
